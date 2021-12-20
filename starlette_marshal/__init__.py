5788333b39b5df7a7c10e1490142f88ac13ea15a
Xurvan <xurvan@tutanota.com>
2021-12-20 05:48:21 +0330

    Add version

--- starlette_marshal/__init__.py
+++ starlette_marshal/__init__.py
@@ -7,3 +7,5 @@ try:
     from starlette_marshal.rapidjson import RapidJSONResponse as JSONResponse
 except ImportError:
     from starlette.responses import JSONResponse  # noqa: F401
+
+__version__ = "0.1.0"
